cb6c7e54d6f29e23327e6791b7c7641de83347d9
Jon Odom <odomj5@gmail.com>
2020-03-04 09:48:57 -0800

    Added icon bar with styling

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -3,6 +3,28 @@ import React from 'react';
 const Splash = () => (
 <div className="splash-div">
     <img src="/assets/GGbridge.jpg" />
+    <div className="icon-bar">
+        <div className='icon-1'>
+            <i className="fas fa-ticket-alt"></i>
+            <div className='icon-1-text'>324,000+ Things to Do</div>
+        </div>
+        <div className='icon-2'>
+            <i className="far fa-star"></i>
+            <div className='icon-2-text'>Millions of Reviews</div>
+        </div>
+        <div className='icon-3'>
+            <i className="fas fa-tag"></i>
+            <div className='icon-3-text'>Lowest Price Guarantee</div>
+        </div>
+        <div className='icon-4'>
+            <i className="fas fa-mobile-alt"></i>
+            <div className='icon-4-text'>24/7 Golobal Support</div>
+        </div>
+        <div className='icon-5'>
+            <i className="fas fa-glasses"></i>
+            <div className='icon-5-text'>A Jonadvisor Company</div>
+        </div>
+    </div>
 </div>
 )
 
