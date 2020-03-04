53b4bf97ce2d65d193ef19f25ba65919e906848e
Jon Odom <odomj5@gmail.com>
2020-03-03 20:09:03 -0800

    Fixed component file structure

--- frontend/components/session_form/login_form_container.js
+++ frontend/components/session_form/login_form_container.js
@@ -1,7 +1,6 @@
-import React from 'react'
 import { connect } from "react-redux";
 import { login } from "../../actions/session_actions"
-import SessionForm from "../session_form/session_form"
+import SessionForm from "./session_form"
 import { openModal, closeModal } from "../../actions/modal_actions";
 
 const mapStateToProps = state => {
