1be99f26db7ce7c2011bb5243937f8e9ce7dacdc
Jon Odom <odomj5@gmail.com>
2020-03-05 11:05:04 -0800

    Updated button cursor style and fixed modal margins

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -58,37 +58,31 @@ class SessionForm extends React.Component {
 
         return (
             <div>
+                <div onClick={this.props.closeModal} className='close-x'>×</div>
                 <form className="Session-Form" onSubmit={this.handleSubmit}>
-                    <div onClick={this.props.closeModal} className='close-x'>×</div>
                     <div className='form-welc'>{this.props.formType === 'signup' ? 'Welcome to Jiator!' : 'Welcome Back!' }</div>
                     <div className='session-errors'>
 
                     {this.renderErrors()}
                     </div>
                     <label >Username:
-                        <br/>
                         <input type="text" autoFocus='autoFocus' value={this.state.username} onChange={this.handleInput("username")} />
                     </label>
-                    <br/>
                     <label>Password:
                         <input type="password" value={this.state.password} onChange={this.handleInput("password")} />
                     </label>
-                    <br />
                     {this.props.formType === 'signup' ? <label>Email:
                         <input type="text" value={this.state.email} onChange={this.handleInput("email")} />
                     </label > : null}
-                    <br />
                     {this.props.formType === 'signup' ? <label>First Name:
                         <input type="text" value={this.state.first_name} onChange={this.handleInput("first_name")} />
                     </label> : null }
-                    <br />
                     {this.props.formType === 'signup' ? <label>Last Name:
                         <input type="text" value={this.state.last_name} onChange={this.handleInput("last_name")} />
                     </label> : null}
                     <button>{this.props.formType === "login" ? "Login" : "Sign Up"}</button>
                     <button className="demo-button" onClick={this.handleDemo}>Demo User</button>
                 </form>
-                <br/>
                 <div className='switch-form'>
                     {this.props.formType === 'signup' ? 'Already have an account?' : 'Need to create an account?'}
                     {this.props.otherForm}
