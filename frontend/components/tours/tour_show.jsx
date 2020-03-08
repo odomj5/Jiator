aa192870e55a68eac63c35441934b6d6ad3e3826
Jon Odom <odomj5@gmail.com>
2020-03-08 13:42:33 -0700

    Started show css

--- frontend/components/tours/tour_show.jsx
+++ frontend/components/tours/tour_show.jsx
@@ -21,39 +21,61 @@ class TourShow extends React.Component {
         // debugger
         return(
             <div className="show-div">
-                <h1>
+                <h1 className="tour-name">
                     {this.props.tour.name}
                 </h1>
-                <img src={this.props.tour.photoUrl}/>
-                <div className="booking-div">
-                    <div>{this.props.tour.price}</div>
-                    <div>{this.props.tour.spacesAvailable}</div>
+                <div className='booking-outer-div'>
+                    <img className="default-img" src={this.props.tour.photoUrl}/>
+                    <div className="booking-div-cont">
+
+                        <div className="booking-div">
+
+                            <div className="booked-div"><i class="fas fa-clipboard-check"></i>Booked by {this.props.tour.spacesAvailable * 13} travelors</div>
+                            <div className="booked-avail-div">
+                                <div>Only {this.props.tour.spacesAvailable} spaces available</div>
+                                <div>From ${this.props.tour.price}</div>
+                                <div>Lowest Price Guarantee</div>
+                            </div>
+                            <div className="booking-calendar">
+                                <div>Date</div>
+                                <div>Number of travelors</div>
+                            </div>
+                            <div className="reservation-dets">
+                                <h2>Reserve Now and Pay Later</h2>
+                                <h3>Secure your spot while staying flexible</h3>
+                                <h2>Free Cancellation</h2>
+                                <h3>Up to 24 hours in advance</h3>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className="details-div">
                     <div className='ttl-bar'>
-                        <div className='ttl-bar-time'>{this.props.tour.timeEstimate}</div>
-                        <div className='ttl-bar-ticket'>Mobile ticket</div>
-                        <div className='ttl-bar-lang'>{this.props.tour.language}</div>
+                        <div className='ttl-bar-time'><i class="far fa-clock"></i> {this.props.tour.timeEstimate}</div>
+                        <div className='ttl-bar-ticket'> <i class="fas fa-envelope-open-text"></i> Mobile ticket</div>
+                        <div className='ttl-bar-lang'> <i class="far fa-comment-dots"></i> Offered in: {this.props.tour.language}</div>
                     </div>
                     <div className="overview-div">
                         <div className="overview-title">Overview</div>
                         <div>
-                            <p>{this.props.tour.overview}</p>
+                            <p className="overview-par">{this.props.tour.overview}</p>
                         </div>
                     </div>
                     <div className="included-div">
                         <div className="included-title">What's Included</div>
-                        <div>{included}</div>
+                        <div className='included-yes'>{included}</div>
+                        
                         {/* <ul className="included-yes">
                             {included.map((feature, idx) => (
                                 <li key="idx">feature</li>))}
                             
                         </ul> */}
-                        <div>{this.props.tour.notIncluded}</div>
+                        <div className='included-no'>{this.props.tour.notIncluded}</div>
                     </div>
                 </div>
                 <div className="additional-div">
-                    <div>{this.props.tour.additionalInfo}</div>
+                    <div className="additional-title">Additional Info</div>
+                    <div className="additional-info">{this.props.tour.additionalInfo}</div>
                 </div>
             </div>
         )
