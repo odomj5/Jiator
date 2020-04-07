04da07f0b0385793bce5227826760ba54be8cda8
Jon Odom <odomj5@gmail.com>
2020-04-07 10:37:53 -0700

    Rendering splash item images

--- frontend/components/splash/splash_index_item.jsx
+++ frontend/components/splash/splash_index_item.jsx
@@ -3,8 +3,12 @@ import React from 'react'
 class SplashIndexItem extends React.Component {
 
     render () {
+        // debugger
         return(
-            <div>Test</div>
+            <div className="splash-idx-item-cont">
+                <img src={this.props.tour.photoUrls[0]}/>
+                {this.props.tour.name}
+            </div>
         )
     }
 }
