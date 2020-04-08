69a67e4867043c23eb4478211aa92b9ce8145990
Jon Odom <odomj5@gmail.com>
2020-04-08 15:12:19 -0700

    Build review form

--- frontend/components/reviews/review_form.jsx
+++ frontend/components/reviews/review_form.jsx
@@ -1,5 +1,64 @@
 import React from "react";
 
 class ReviewForm extends React.Component {
-    
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: "", 
+            body: "",
+            rating: ""
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        let review = Object.assign({}, this.state)
+        review.rating = parseInt(this.state.rating)
+        this.props.createReview(this.props.tourId, review)
+    }
+
+    handleInput(inputType) {
+        return e => this.setState({[inputType]: e.target.value})
+    }
+
+    render() {
+        return(
+            <div className="review-form">
+                <form onSubmit={this.handleSubmit}>
+                    <label>Title:
+                        <input 
+                            type="text"
+                            value={this.state.title}
+                            onChange={this.handleInput("title")}
+                        />
+                    </label>
+                    <label>Body:
+                        <input
+                            type="text"
+                            value={this.state.body}
+                            onChange={this.handleInput("body")}
+                        />
+                    </label>
+                    <label>Rating:
+                        <select 
+                            value={this.state.rating}
+                            onChange={this.handleInput("rating")}
+                            >
+                            <option value="5">5</option>
+                            <option value="4">4</option>
+                            <option value="3">3</option>
+                            <option value="2">2</option>
+                            <option value="1">1</option>
+                        </select>
+                    </label>
+                    <button >Submit Review</button>
+                </form>
+            </div>
+        )
+    }
 }
+
+
+export default ReviewForm; 
