7aefaafc26c046a0858d7a7104a0adf66a9117da
Jon Odom <odomj5@gmail.com>
2020-04-14 14:39:28 -0700

    Reversed reviews order

--- frontend/components/reviews/review.jsx
+++ frontend/components/reviews/review.jsx
@@ -8,14 +8,19 @@ class Review extends React.Component {
         this.props.requestReviews(this.props.tour.id)
     }
 
+    
+
     render() {
         if (this.props.reviews === undefined) return null 
+
+        const reviewsRev = this.props.reviews.reverse() 
+
         return(
             <div className="review-div">
                 <div className="review-header">Reviews
                 <button onClick={() => this.props.openModal("review")}>Leave A Review</button>
-
-                {this.props.reviews.map(review => 
+                
+                {reviewsRev.map(review => 
                 <ReviewIndex 
                     key={review.id}
                     review={review}
