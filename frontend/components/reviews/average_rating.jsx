e085cdd9581a5b385c8dc21d757e044ac40763e8
Jon Odom <odomj5@gmail.com>
2020-04-20 17:03:36 -0700

    Created tour rating average function

--- frontend/components/reviews/average_rating.jsx
+++ frontend/components/reviews/average_rating.jsx
@@ -3,15 +3,26 @@ import React from 'react';
 
 class AverageRating extends React.Component {
 
-    // tourAvg() {
-
-    // }
+    tourAvg() {
+        const allReviews = this.props.reviews
+        const reviewCount = allReviews.length
+        let reviewSum = 0
+        allReviews.map((review, idx) => {
+            let rating = review.rating
+            reviewSum += rating
+            debugger
+        })
+        debugger
+        const ratingAvg = (reviewSum / reviewCount)
+        return reviewCount
+    }
 
 
     render() {
         return(
             <div className="avg-rating-div">
                 <h1>test</h1>
+                {this.tourAvg()}
             </div>
         )
     } 
