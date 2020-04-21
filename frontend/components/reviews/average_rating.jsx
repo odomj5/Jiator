d32f5e84f151b32d37395e8a78cdc1fe42a97c79
Jon Odom <odomj5@gmail.com>
2020-04-20 17:21:54 -0700

    Updated tour average variables

--- frontend/components/reviews/average_rating.jsx
+++ frontend/components/reviews/average_rating.jsx
@@ -2,18 +2,22 @@ import React from 'react';
 
 
 class AverageRating extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         ratingAverage: this.props.reviews.map(review => review.rating)
+    //     }
+    //     debugger
+    // }
+
 
     tourAvg() {
         const allReviews = this.props.reviews
         const reviewCount = allReviews.length
-        let reviewSum = 0
-        allReviews.map((review, idx) => {
-            let rating = review.rating
-            reviewSum += rating
-            debugger
-        })
+        const ratings = this.props.reviews.map(review => review.rating)
+        const ratingSum = ratings.sum()
         debugger
-        const ratingAvg = (reviewSum / reviewCount)
+        const ratingAvg = (ratingSum / reviewCount)
         return reviewCount
     }
 
